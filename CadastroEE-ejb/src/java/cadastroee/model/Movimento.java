477b9290f2d63ebc118d4fae7958ad9320daaaec
Marcelo Rosa Fontes <131271249+fontesthebest@users.noreply.github.com>
2024-05-25 11:19:03 -0300

    Add files via upload

---
+++ CadastroEE-ejb/src/java/cadastroee/model/Movimento.java
@@ -0,0 +1,147 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cadastroee.model;
+
+import jakarta.persistence.Basic;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.NamedQueries;
+import jakarta.persistence.NamedQuery;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import jakarta.xml.bind.annotation.XmlRootElement;
+import java.io.Serializable;
+
+/**
+ *
+ * @author naote
+ */
+@Entity
+@Table(name = "movimento", catalog = "loja", schema = "dbo")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Movimento.findAll", query = "SELECT m FROM Movimento m"),
+    @NamedQuery(name = "Movimento.findByIdMovimento", query = "SELECT m FROM Movimento m WHERE m.idMovimento = :idMovimento"),
+    @NamedQuery(name = "Movimento.findByQuantidade", query = "SELECT m FROM Movimento m WHERE m.quantidade = :quantidade"),
+    @NamedQuery(name = "Movimento.findByTipo", query = "SELECT m FROM Movimento m WHERE m.tipo = :tipo"),
+    @NamedQuery(name = "Movimento.findByValorUnitario", query = "SELECT m FROM Movimento m WHERE m.valorUnitario = :valorUnitario")})
+public class Movimento implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "idMovimento", nullable = false)
+    private Integer idMovimento;
+    @Column(name = "quantidade")
+    private Integer quantidade;
+    @Size(max = 5)
+    @Column(name = "tipo", length = 5)
+    private String tipo;
+    @Column(name = "valorUnitario")
+    private Long valorUnitario;
+    @JoinColumn(name = "idPessoa", referencedColumnName = "idPessoa")
+    @ManyToOne
+    private Pessoa idPessoa;
+    @JoinColumn(name = "idProduto", referencedColumnName = "idProduto")
+    @ManyToOne
+    private Produtos idProduto;
+    @JoinColumn(name = "idUsuario", referencedColumnName = "idUsuario")
+    @ManyToOne
+    private Usuario idUsuario;
+
+    public Movimento() {
+    }
+
+    public Movimento(Integer idMovimento) {
+        this.idMovimento = idMovimento;
+    }
+
+    public Integer getIdMovimento() {
+        return idMovimento;
+    }
+
+    public void setIdMovimento(Integer idMovimento) {
+        this.idMovimento = idMovimento;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public Long getValorUnitario() {
+        return valorUnitario;
+    }
+
+    public void setValorUnitario(Long valorUnitario) {
+        this.valorUnitario = valorUnitario;
+    }
+
+    public Pessoa getIdPessoa() {
+        return idPessoa;
+    }
+
+    public void setIdPessoa(Pessoa idPessoa) {
+        this.idPessoa = idPessoa;
+    }
+
+    public Produtos getIdProduto() {
+        return idProduto;
+    }
+
+    public void setIdProduto(Produtos idProduto) {
+        this.idProduto = idProduto;
+    }
+
+    public Usuario getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(Usuario idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idMovimento != null ? idMovimento.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Movimento)) {
+            return false;
+        }
+        Movimento other = (Movimento) object;
+        if ((this.idMovimento == null && other.idMovimento != null) || (this.idMovimento != null && !this.idMovimento.equals(other.idMovimento))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "cadastroee.model.Movimento[ idMovimento=" + idMovimento + " ]";
+    }
+    
+}
---
+++ CadastroEE-ejb/src/java/cadastroee/model/Produtos.java
@@ -0,0 +1,126 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cadastroee.model;
+
+import jakarta.persistence.Basic;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.NamedQueries;
+import jakarta.persistence.NamedQuery;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import jakarta.xml.bind.annotation.XmlRootElement;
+import jakarta.xml.bind.annotation.XmlTransient;
+import java.io.Serializable;
+import java.util.Collection;
+
+/**
+ *
+ * @author naote
+ */
+@Entity
+@Table(name = "produtos", catalog = "loja", schema = "dbo")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Produtos.findAll", query = "SELECT p FROM Produtos p"),
+    @NamedQuery(name = "Produtos.findByIdProduto", query = "SELECT p FROM Produtos p WHERE p.idProduto = :idProduto"),
+    @NamedQuery(name = "Produtos.findByNome", query = "SELECT p FROM Produtos p WHERE p.nome = :nome"),
+    @NamedQuery(name = "Produtos.findByQuantidade", query = "SELECT p FROM Produtos p WHERE p.quantidade = :quantidade"),
+    @NamedQuery(name = "Produtos.findByPrecoVenda", query = "SELECT p FROM Produtos p WHERE p.precoVenda = :precoVenda")})
+public class Produtos implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "idProduto", nullable = false)
+    private Integer idProduto;
+    @Size(max = 255)
+    @Column(name = "nome", length = 255)
+    private String nome;
+    @Column(name = "quantidade")
+    private Integer quantidade;
+    @Column(name = "precoVenda")
+    private Long precoVenda;
+    @OneToMany(mappedBy = "idProduto")
+    private Collection<Movimento> movimentoCollection;
+
+    public Produtos() {
+    }
+
+    public Produtos(Integer idProduto) {
+        this.idProduto = idProduto;
+    }
+
+    public Integer getIdProduto() {
+        return idProduto;
+    }
+
+    public void setIdProduto(Integer idProduto) {
+        this.idProduto = idProduto;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public Long getPrecoVenda() {
+        return precoVenda;
+    }
+
+    public void setPrecoVenda(Long precoVenda) {
+        this.precoVenda = precoVenda;
+    }
+
+    @XmlTransient
+    public Collection<Movimento> getMovimentoCollection() {
+        return movimentoCollection;
+    }
+
+    public void setMovimentoCollection(Collection<Movimento> movimentoCollection) {
+        this.movimentoCollection = movimentoCollection;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idProduto != null ? idProduto.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Produtos)) {
+            return false;
+        }
+        Produtos other = (Produtos) object;
+        if ((this.idProduto == null && other.idProduto != null) || (this.idProduto != null && !this.idProduto.equals(other.idProduto))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "cadastroee.model.Produtos[ idProduto=" + idProduto + " ]";
+    }
+    
+}
